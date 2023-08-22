f78191dc02bce6b7838dcc844bdf31402f9e1fb2
egoitzct <egoitzcarral@gmail.com>
2023-08-22 16:30:01 +0200

    Update variable_nominal_analisis.R

--- R/variable_nominal_analisis.R
+++ R/variable_nominal_analisis.R
@@ -7,4 +7,10 @@ variable_nominal_analisis <- function(variable, frecuencia = TRUE, grafico = TRU
     epiDisplay::tab1(variable, sort.group = "decreasing", graph = grafico,
                      main = "Distribución de la variable.")
   }
+
+  if (frecuencia != TRUE && grafico == TRUE) {
+    frecuen <- epiDisplay::tab1(variable, sort.group = "decreasing", graph = grafico,
+                                main = "Distribución de la variable.")
+
+  }
 }
