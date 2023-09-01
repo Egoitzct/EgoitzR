72f32fa5bc094fb32c50766700c28e0a2ff3f4e0
egoitzct <egoitzcarral@gmail.com>
2023-09-01 14:07:14 +0200

    0.2.2
    Update R/tabla_contingencia.R

--- R/tabla_contingencia.R
+++ R/tabla_contingencia.R
@@ -1,5 +1,6 @@
 #' @title Tabla de Contingencia
-#'
+#' @param var1 Variable que se quiera introducir para formar una tabla de contingencia
+#' @param var2 Variable que se quiera introducir para formar una tabla de contingencia
 #' @export
 
 tabla_contingencia <- function(var1, var2) {
@@ -9,6 +10,8 @@ tabla_contingencia <- function(var1, var2) {
 
   tabla_base_2
 }
+
+#' @param tabla Tabla usada para calcular los coeficientes Chi-Cuadrado, Phi, V de Cramer y el coeficiente de contingencia
 
 phi_coef <- function(tabla) {
   unname(sqrt(chisq.test(tabla)$statistic / sum(tabla)))
@@ -22,26 +25,26 @@ coef_conting <- function(tabla) {
   unname(sqrt(chisq.test(tabla)$statistic / (chisq.test(tabla)$statistic + sum(tabla))))
 }
 
-chi_cuadrado <- function(var1, var2, phi_coef = FALSE, coef_conting = TRUE,
+chi_cuadrado <- function(tabla, phi_coef = FALSE, coef_conting = TRUE,
                          cramer_v = TRUE) {
   suppressWarnings({
 
-  chi <- chisq.test(table(var1, var2))
+  chi <- chisq.test(tabla)
 
   if (phi_coef) {
-    phi <- phi_coef(table(var1, var2))
+    phi <- phi_coef(tabla)
   } else {
     phi <- "FALSE"
   }
 
   if (cramer_v) {
-    cramer <- cramer_v(table(var1, var2))
+    cramer <- cramer_v(tabla)
   } else {
     cramer <- "FALSE"
   }
 
   if (coef_conting) {
-    contingencia <- coef_conting(table(var1, var2))
+    contingencia <- coef_conting(tabla)
   } else {
     contingencia <- "FALSE"
   }
